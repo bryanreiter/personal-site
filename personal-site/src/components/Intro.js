67d3e4469ac65531ceeffeaf23a86abfcb4c7da6
bryanreiter <91745125+bryanreiter@users.noreply.github.com>
2024-05-02 01:00:56 -0400

    started intro section

--- personal-site/src/components/Intro.js
+++ personal-site/src/components/Intro.js
@@ -1,3 +1,13 @@
 export default function Intro() {
-  return <section id="intro" className="py-16 bg-gray-100"></section>;
+  return (
+    <section id="intro" className="py-16 bg-gray-100">
+      <div className="container mx-auto px-4">
+        <h1 className="text-4xl font-bold mb-8">
+          Hello, I'm Bryan Reiter.
+          <br />A recently graduated Software Engineer <br />
+          from Temple University with a degree in Computer Science
+        </h1>
+      </div>
+    </section>
+  );
 }
