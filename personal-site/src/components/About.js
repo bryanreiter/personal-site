537174a79e4850a936f85116dcef73feccbe0fe1
bryanreiter <91745125+bryanreiter@users.noreply.github.com>
2024-05-04 19:22:59 -0400

    removed background color

--- personal-site/src/components/About.js
+++ personal-site/src/components/About.js
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <section id="about" className="py-16 bg-gray-100">
+    <section id="about" className="py-16">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         </div>
