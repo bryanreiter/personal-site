865e74a83005cb3295c9f4c59be245a17289c713
bryanreiter <91745125+bryanreiter@users.noreply.github.com>
2024-05-04 18:38:10 -0400

    added typewriter effect

--- personal-site/src/components/Intro.js
+++ personal-site/src/components/Intro.js
@@ -1,11 +1,18 @@
+import Typewriter from "typewriter-effect";
+
 export default function Intro() {
   return (
-    <section id="intro" className="py-16 bg-gray-100">
-      <div className="container mx-auto px-4">
+    <section id="intro" className="py-16 bg-gray-900">
+      <div className="container mx-auto px-4 content-center">
         <h1 className="text-4xl font-bold mb-8">
-          Hello, I'm Bryan Reiter.
-          <br />A recently graduated Software Engineer <br />
-          from Temple University with a degree in Computer Science
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(`Hey, I'm <span style="color: #4c1d95;">Bryan Reiter!</span>`)
+                .callFunction(() => {})
+                .start();
+            }}
+          />
         </h1>
       </div>
     </section>
