c71affdf273055ece9cd6c87151a30f3d18d43d9
bryanreiter <91745125+bryanreiter@users.noreply.github.com>
2024-05-04 18:38:01 -0400

    update colors and format

--- personal-site/src/components/Navbar.js
+++ personal-site/src/components/Navbar.js
@@ -1,26 +1,42 @@
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center py-4">
-      <p className="text-2xl font-bold text-gray-800">Logo</p>
-      <div className="flex space-x-4">
-        <a href="#home" className="text-gray-800">
-          Home
+    <nav className="bg-gray-800 py-4 fixed top-0 left-0 right-0">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <a href="#intro" className="text-white text-xl font-bold">
+          Logo
         </a>
-        <a href="#intro" className="text-gray-800">
-          Intro
-        </a>
-        <a href="#about" className="text-gray-800">
-          About
-        </a>
-        <a href="#skills" className="text-gray-800">
-          Skills
-        </a>
-        <a href="#projects" className="text-gray-800">
-          Projects
-        </a>
-        <a href="#contact" className="text-gray-800">
-          Contact
-        </a>
+        <button className="md:hidden text-white focus:outline-none">
+          <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path
+              d="M4 6h16M4 12h16M4 18h16"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+        <ul className="hidden md:flex space-x-4">
+          <li>
+            <a href="#about" className="text-white hover:text-violet-900 font-bold">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="text-white hover:text-violet-900 font-bold">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="text-white hover:text-violet-900 font-bold">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="text-white hover:text-violet-900 font-bold">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
