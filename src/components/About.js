bea839e369fdeec4e7c2bb62b2e3a0a37de4f830
Bryan Reiter <tul13138@temple.edu>
2024-05-06 00:44:30 -0400

    Started About page, added shadcnui cards

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,12 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function About() {
   return (
     <section
@@ -5,7 +14,32 @@ export default function About() {
       className="min-h-screen flex items-center justify-center py-16 relative"
     >
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8"></div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <Card className="bg-zinc-700 shadow-lg shadow-purple-600 border-purple-600 text-white">
+            <CardHeader>
+              <CardTitle>Background</CardTitle>
+              <CardDescription className="text-white">Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card className="bg-zinc-700 shadow-lg shadow-purple-600 border-purple-600 text-white">
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription className="text-white">Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </section>
   );
