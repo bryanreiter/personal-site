12248382f730fea9b3b90b5a6ddc6843842658ff
Bryan Reiter <tul13138@temple.edu>
2024-05-06 13:20:56 -0400

    started about me section

--- src/components/About.js
+++ src/components/About.js
@@ -14,31 +14,55 @@ export default function About() {
       className="min-h-screen flex items-center justify-center py-16 relative"
     >
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <Card className="bg-zinc-700 shadow-lg shadow-purple-600 border-purple-600 text-white">
-            <CardHeader>
-              <CardTitle>Background</CardTitle>
-              <CardDescription className="text-white">Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-          <Card className="bg-zinc-700 shadow-lg shadow-purple-600 border-purple-600 text-white">
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription className="text-white">Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+          <div>
+            <Card className="bg-zinc-700 shadow-lg shadow-midnightPurple border-midnightPurple text-white">
+              <CardHeader>
+                <CardTitle>Education</CardTitle>
+                <CardDescription className="text-white">
+                  Bachelor of Science in Computer Science
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  In May 2024, I graduated from Temple University's College of
+                  Science and Technology with a degree in Computer Science
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+          <div>
+            <Card className="bg-zinc-700 shadow-lg shadow-midnightPurple border-midnightPurple text-white">
+              <CardHeader>
+                <CardTitle>Master Data Management Intern</CardTitle>
+                <CardDescription className="text-white">
+                    <em>Victaulic</em>
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>May 2023 – Present</p>
+              </CardFooter>
+            </Card>
+          </div>
+          <div>
+            <Card className="bg-zinc-700 shadow-lg shadow-midnightPurple border-midnightPurple text-white">
+              <CardHeader>
+                <CardTitle>Background</CardTitle>
+                <CardDescription className="text-white">
+                  Card Description
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
       </div>
     </section>
