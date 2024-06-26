798f760e0de03b9813568150fb0209113a634608
bryanreiter <91745125+bryanreiter@users.noreply.github.com>
2024-06-26 16:17:50 -0400

    new sexier cards, fix some mobile layout issues

--- src/components/About.js
+++ src/components/About.js
@@ -9,61 +9,116 @@ import {
 
 export default function About() {
   return (
-    <section
-      id="about"
-      className="min-h-screen flex items-center justify-center py-16 relative"
-    >
+    <section id="about" className="py-16 md:py-24 relative">
       <div
-        className="container mx-auto px-4"
+        className="container mx-auto px-4 md:px-8"
         style={{ fontFamily: "monospace" }}
       >
         <h2 className="text-4xl font-bold text-center mb-8">About Me</h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
-          <div>
-            <Card className="bg-zinc-700 shadow-lg shadow-midnightPurple border-midnightPurple text-white">
-              <CardHeader>
-                <CardTitle>Bachelor of Science in Computer Science</CardTitle>
-                <CardDescription className="text-white">
-                  <em>Temple University - May 2024</em>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+          <div className="flex flex-col h-full">
+            <Card className="border-none bg-gradient-to-r from-midnightPurple to-blue-500 flex flex-col h-full">
+              <CardHeader className="p-4">
+                <CardTitle className="text-lg md:text-xl">
+                  Temple University
+                </CardTitle>
+                <CardDescription className="text-white text-sm md:text-base">
+                  <em>Bachelor of Science in Computer Science</em>
                 </CardDescription>
               </CardHeader>
-              <CardContent>
-                <p>
-                  I graduated from Temple University's College of Science and
-                  Technology with a degree in Computer Science in May 2024.
-                </p>
+              <CardContent className="text-white flex-grow text-sm md:text-base p-4">
+                <ul className="list-disc pl-5">
+                  <li>Graduated with a 3.55 GPA</li>
+                  <li>
+                    Key coursework: Data Structures & Algorithms, Web
+                    Application Development, Systems Prog. & Operating Systems,
+                    Software Design
+                  </li>
+                  <li>
+                    Developed strong problem-solving and coding skills in
+                    various languages
+                  </li>
+                  <li>Community Lead for Temple University Esports</li>
+                  <li>
+                    Led 24-Hour 'Gaming For Good' Charity Live Stream, raising
+                    over $300
+                  </li>
+                </ul>
               </CardContent>
-            </Card>
-          </div>
-          <div>
-            <Card className="bg-zinc-700 shadow-lg shadow-midnightPurple border-midnightPurple text-white">
-              <CardHeader>
-                <CardTitle>Background</CardTitle>
-                <CardDescription className="text-white">
-                  Card Description
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
+              <CardFooter className="text-white mt-auto p-4">
+                <em>August 2020 - May 2024</em>
               </CardFooter>
             </Card>
           </div>
-          <div>
-            <Card className="bg-zinc-700 shadow-lg shadow-midnightPurple border-midnightPurple text-white">
+          <div className="flex flex-col h-full">
+            <Card className="border-none bg-gradient-to-r from-midnightPurple to-blue-500 flex flex-col h-full">
               <CardHeader>
-                <CardTitle>Personal Interests/Hobbies</CardTitle>
+                <CardTitle>Victaulic Internship</CardTitle>
+                <CardDescription className="text-white">
+                  <em>Master Data Management Intern at Victaulic</em>
+                </CardDescription>
               </CardHeader>
-              <CardContent className="list-disc flex flex-row">
-                <ul>
+              <CardContent className="text-white flex-grow">
+                <ul className="list-disc pl-5">
+                  <li>
+                    Created business rules for data governance in MDM Syndigo
+                    System
+                  </li>
+                  <li>
+                    Developed custom reports using Elasticsearch Query DSL
+                  </li>
+                  <li>
+                    Improved MDM notification system and audited existing
+                    notifications
+                  </li>
+                  <li>
+                    Worked with real-time analytics for error processing and
+                    detection
+                  </li>
+                </ul>
+              </CardContent>
+              <CardFooter className="text-white mt-auto">
+                <em>May 2023 - June 2024</em>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="flex flex-col h-full">
+            <Card className="border-none bg-gradient-to-r from-midnightPurple to-blue-500 flex flex-col h-full">
+              <CardHeader>
+                <CardTitle>Temple University IT Services</CardTitle>
+                <CardDescription className="text-white flex-grow">
+                  <em>Help Desk Technical Assistant</em>
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="text-white">
+                <ul className="list-disc pl-5">
+                  <li>
+                    Provided technical support to students, alumni, and staff
+                  </li>
+                  <li>Resolved software and hardware issues</li>
+                  <li>Collaborated with team and trained new employees</li>
+                </ul>
+              </CardContent>
+              <CardFooter className="text-white mt-auto">
+                <em>January 2023 - May 2024</em>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="flex flex-col h-full">
+            <Card className="border-none bg-gradient-to-r from-midnightPurple to-blue-500 flex flex-col h-full">
+              <CardHeader>
+                <CardTitle></CardTitle>
+                <CardDescription className="text-white flex-grow"></CardDescription>
+              </CardHeader>
+              <CardContent className="text-white">
+                <ul className="list-disc pl-5">
                   <li>Video Games</li>
                   <li>Music</li>
                   <li>Web Development</li>
                   <li>Programming</li>
                 </ul>
               </CardContent>
+              <CardFooter className="text-white mt-auto"></CardFooter>
             </Card>
           </div>
         </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -19,86 +19,110 @@ export default function Intro() {
   const [showPhone, setShowPhone] = useState(false);
 
   return (
-    <section
-      id="intro"
-      className="min-h-screen flex flex-col justify-center py-16 px-4 relative"
-    >
-      <div className="container mx-auto text-center">
-      <div className="max-w-m mx-auto">
-        <h1 className="text-2xl md:text-4xl font-bold mb-8">
-          <div style={{ fontFamily: "monospace" }}>
-            <Typewriter
-              options={{
-                delay: 30,
-              }}
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString(
-                    `Hey, I'm <span style="color: #6d28d9;">Bryan Reiter</span>,<br />a Computer Science Graduate<br />from Temple University.<br /><span style="font-size: 1rem md:font-size: 1.5rem;">I'm a <span style="color: #6d28d9;">software developer</span> and <span style="color: #6d28d9;">video game nerd</span> at heart.<br /><br /><span style="color: #6d28d9;">Scroll</span> to get started<br />or <span style="color: #6d28d9;">choose how to connect</span> below:</span>`
-                  )
-                  .callFunction(() => {
-                    setTimeout(() => setShowGithub(true), 500);
-                    setTimeout(() => setShowLinkedin(true), 1000);
-                    setTimeout(() => setShowInstagram(true), 1500);
-                    setTimeout(() => setShowDiscord(true), 2000);
-                    setTimeout(() => setShowEnvelope(true), 2500);
-                    setTimeout(() => setShowPhone(true), 3000);
-                  })
-                  .start();
-              }}
-            />
+      <section
+        id="intro"
+        className="h-screen flex flex-col justify-center py-16 px-4 relative mb-16 md:mb-24"
+      >
+        <div className="container mx-auto text-center">
+          <div className="max-w-m mx-auto">
+            <h1 className="text-xl md:text-2xl lg:text-4xl font-bold mb-8">
+              <div style={{ fontFamily: "monospace" }}>
+                <Typewriter
+                  options={{
+                    delay: 30,
+                  }}
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString(
+                        `Hey, I'm <span style="color: #6d28d9;">Bryan Reiter</span>,<br />a Computer Science Graduate<br />from Temple University.<br /><span style="font-size: 1rem md:font-size: 1.5rem;">I'm a <span style="color: #6d28d9;">software developer</span> and <span style="color: #6d28d9;">video game nerd</span> at heart.<br /><br /><span style="color: #6d28d9;">Scroll</span> to get started<br />or <span style="color: #6d28d9;">choose how to connect</span> below:</span>`
+                      )
+                      .callFunction(() => {
+                        setTimeout(() => setShowGithub(true), 500);
+                        setTimeout(() => setShowLinkedin(true), 1000);
+                        setTimeout(() => setShowInstagram(true), 1500);
+                        setTimeout(() => setShowDiscord(true), 2000);
+                        setTimeout(() => setShowEnvelope(true), 2500);
+                        setTimeout(() => setShowPhone(true), 3000);
+                      })
+                      .start();
+                  }}
+                />
+              </div>
+            </h1>
           </div>
-        </h1>
+          <div className="flex flex-wrap justify-center gap-5">
+            <div className={`icon ${showGithub ? "show" : ""}`}>
+              <a
+                href="https://github.com/bryanreiter"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  size="2x"
+                  className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple"
+                />
+              </a>
+            </div>
+            <div className={`icon ${showLinkedin ? "show" : ""}`}>
+              <a
+                href="https://www.linkedin.com/in/bryanreiter/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  size="2x"
+                  className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple"
+                />
+              </a>
+            </div>
+            <div className={`icon ${showInstagram ? "show" : ""}`}>
+              <a
+                href="https://www.instagram.com/bryanreiter_/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  size="2x"
+                  className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple"
+                />
+              </a>
+            </div>
+            <div className={`icon ${showDiscord ? "show" : ""}`}>
+              <a
+                href="https://discordapp.com/users/271769741457227776"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon
+                  icon={faDiscord}
+                  size="2x"
+                  className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple"
+                />
+              </a>
+            </div>
+            <div className={`icon ${showEnvelope ? "show" : ""}`}>
+              <a href="mailto:bryanreiter2002@gmail.com">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  size="2x"
+                  className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple"
+                />
+              </a>
+            </div>
+            <div className={`icon ${showPhone ? "show" : ""}`}>
+              <a href="tel:484-268-4900">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  size="2x"
+                  className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple"
+                />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-wrap justify-center gap-5">
-          <div className={`icon ${showGithub ? "show" : ""}`}>
-            <a
-              href="https://github.com/bryanreiter"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FontAwesomeIcon icon={faGithub} size="2x" className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple"/>
-            </a>
-          </div>
-          <div className={`icon ${showLinkedin ? "show" : ""}`}>
-            <a
-              href="https://www.linkedin.com/in/bryanreiter/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FontAwesomeIcon icon={faLinkedin} size="2x" className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple" />
-            </a>
-          </div>
-          <div className={`icon ${showInstagram ? "show" : ""}`}>
-            <a
-              href="https://www.instagram.com/bryanreiter_/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FontAwesomeIcon icon={faInstagram} size="2x" className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple" />
-            </a>
-          </div>
-          <div className={`icon ${showDiscord ? "show" : ""}`}>
-            <a
-              href="https://discordapp.com/users/271769741457227776"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FontAwesomeIcon icon={faDiscord} size="2x" className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple" />
-            </a>
-          </div>
-          <div className={`icon ${showEnvelope ? "show" : ""}`}>
-            <a href="mailto:bryanreiter2002@gmail.com">
-              <FontAwesomeIcon icon={faEnvelope} size="2x" className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple" />
-            </a>
-          </div>
-          <div className={`icon ${showPhone ? "show" : ""}`}>
-            <a href="tel:484-268-4900">
-              <FontAwesomeIcon icon={faPhone} size="2x" className="transition duration-300 ease-in-out transform hover:scale-125 hover:text-midnightPurple" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
   );
 }
