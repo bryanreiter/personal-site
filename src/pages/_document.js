d8703ef0ae266e5732635d30323203b088d4e252
bryanreiter <91745125+bryanreiter@users.noreply.github.com>
2024-07-03 17:26:14 -0400

    added title and some metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>Bryan Reiter</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="Bryan Reiter Portfolio Website" />
+      <meta name="author" content="Bryan Reiter" />
+      <meta name="theme-color" content="#6d28d9" />
+      <meta name="robots" content="index, follow" />
+      </Head>
       <body>
         <Main />
         <NextScript />
